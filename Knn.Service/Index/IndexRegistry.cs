2846122cccf29491f379cdc4535aac783d6b85d1
2ooom <dmitry.parfenchik@gmail.com>
2019-03-29 21:43:20 +0100

    Adding Service project

---
+++ Knn.Service/Index/IndexRegistry.cs
@@ -0,0 +1,17 @@
+﻿using HNSW.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Knn.Service.Index
+{
+    public class IndexRegistry
+    {
+        private Dictionary<int, SmallWorld<float[], float>> _indices;
+        public IndexRegistry()
+        {
+            
+        }
+    }
+}
---
+++ Knn.Service/Program.cs
@@ -0,0 +1,23 @@
+﻿using HNSW.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Knn.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
